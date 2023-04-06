c1435f43ed9a36d205935e23d933e2d7af96c769
Namjun-Lee <123014330+Namjun-Lee@users.noreply.github.com>
2023-04-06 09:02:35 -0700

    Add files via upload

---
+++ parrot.py
@@ -0,0 +1,7 @@
+class Parrot:
+    def __init__(self, model_tag):
+        self.model_tag = model_tag
+    
+    def augment(self, input_paraphrase, use_gpu, do_diverse, max_return_phrases, max_length, adequacy_threshold, fluency_threshold):
+        # Implementation of the `augment` method goes here
+        pass
